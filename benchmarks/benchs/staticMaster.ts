9765777fefa1135b57f9574f3193606e590e5276
Isaac Besora <isaac.besora@gmail.com>
2021-11-11 09:22:24 +0100

    Uses local pbf files

--- benchmarks/benchs/staticMaster.ts
+++ benchmarks/benchs/staticMaster.ts
@@ -3,7 +3,7 @@ import { Static } from "protomaps";
 export default class StaticMap extends Benchmark {
   async bench() {
     const map = new Static({
-      url: "https://api.protomaps.com/tiles/v2/{z}/{x}/{y}.pbf?key=1003762824b9687f",
+      url: "http://localhost:9966/data/tiles/{z}/{x}/{y}.pbf",
     });
     const mapContainer = document.getElementById("map-container-master");
     await map.drawCanvas(mapContainer, [37.807, -122.271], 14);
