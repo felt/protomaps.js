4036e6974881a266339d1c4ff8509ad658cc334a
Isaac Besora <isaac.besora@gmail.com>
2022-06-07 13:11:02 +0200

    Cleans dist after testing

--- dist/workaround.d.ts
+++
@@ -1,4 +0,0 @@
-import Point from "@mapbox/point-geometry";
-export declare const simplify: (rings: number[][][], pointsToKeep: number) => number[][][];
-export declare const splitMultiLineString: (mls: Point[][], maxVertices: number) => any[][][];
-export declare const splitMultiPolygon: (mp: Point[][], maxVertices: number) => any[][][];
